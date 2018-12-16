06b56e35c1263111f6d90fe453be82295abe3790
Tonyjlum <tonyjlum@gmail.com>
2018-12-15 23:16:51 -0500

    create page with new.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :new, :create]
+  resources :students, only: [:index, :new]
   get '/student/:id', to: 'students#show', as: 'student'
+  post "/student", to: 'students#create'
+
 end
